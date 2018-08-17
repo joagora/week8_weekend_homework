0ec7fff33e733e1565ecb992883a14500f35770f
Joanna Gora <joanna.gora@protonmail.com>
2018-08-17 13:17:11 +0100

    create Actor class

--- src/main/java/models/Film.java
+++ src/main/java/models/Film.java
@@ -1,4 +1,58 @@
 package models;
 
+import java.util.List;
+
 public class Film {
+
+    private int id;
+    private double budget;
+    private String title;
+    private Director director;
+    private List<Actor> cast;
+
+    public Film(double budget, String title, Director director) {
+        this.budget = budget;
+        this.title = title;
+        this.director = director;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+
+    public void setBudget(double budget) {
+        this.budget = budget;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Director getDirector() {
+        return director;
+    }
+
+    public void setDirector(Director director) {
+        this.director = director;
+    }
+
+    public List<Actor> getCast() {
+        return cast;
+    }
+
+    public void setCast(List<Actor> cast) {
+        this.cast = cast;
+    }
 }
