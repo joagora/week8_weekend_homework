743c725574363c3249322d2194ed248573344493
Joanna Gora <joanna.gora@protonmail.com>
2018-08-17 13:25:50 +0100

    created enums for CategoryType and AwardType

---
+++ src/main/java/models/AwardType.java
@@ -0,0 +1,8 @@
+package models;
+
+public enum AwardType {
+
+    GOLDEN_GLOBES,
+    OSCARS,
+    BRITISH_ACADEMY
+}
---
+++ src/main/java/models/CategoryType.java
@@ -0,0 +1,12 @@
+package models;
+
+public enum CategoryType {
+    BEST_ACTOR,
+    BEST_ACTRESS,
+    BEST_PICTURE,
+    BEST_MUSIC,
+    BEST_CINEMATOGRAPHY,
+    BEST_COSTUME_DESIGN,
+    BEST_SPECIAL_EFFECTS;
+
+}
